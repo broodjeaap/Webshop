a6daaa6ae895254acd6f063ece8ac629c3ec8bf0
David van Zessen <davidzessen@gmail.com>
2013-03-20 13:05:13 +0100

    categories klaar
    categories klaar, misschien nog wat grafische aanpassingen
    "foto & video" category geeft geen resultaten (waarschijnlijk door '&')
    "nieuwe" layout, alles verwijderd behalve wat nodig is voor een
    header/footer/sidebar/content div

--- Webshop/Controllers/HomeController.cs
+++ Webshop/Controllers/HomeController.cs
@@ -15,9 +15,25 @@ namespace Webshop.Controllers
 
         private WebshopContext db = new WebshopContext();
 
-        public ActionResult Index()
+        public ActionResult Index(string category = "", string subcat1 = "", string subcat2 = "")
         {
-            return View(db.Products.Take(100).AsEnumerable());
+            ViewBag.category = category;
+            ViewBag.subcat1 = subcat1;
+            ViewBag.subcat2 = subcat2;
+            List<Product> filteredProducts = db.Products.ToList();
+            if (!category.Equals(""))
+            {
+                filteredProducts = filteredProducts.Where(p => p.Category.Equals(category)).ToList();
+            }
+            if (!subcat1.Equals(""))
+            {
+                filteredProducts = filteredProducts.Where(p => p.SubCat1.Equals(subcat1)).ToList();
+            }
+            if (!subcat2.Equals(""))
+            {
+                filteredProducts = filteredProducts.Where(p => p.SubCat2.Equals(subcat2)).ToList();
+            }
+            return View(filteredProducts.Take(20));
         }
 
         public ActionResult About()
@@ -27,8 +43,11 @@ namespace Webshop.Controllers
             return View();
         }
 
-        public ActionResult Product(int id)
+        public ActionResult Product(int id, string category = "", string subcat1 = "", string subcat2 = "")
         {
+            ViewBag.category = category;
+            ViewBag.subcat1 = subcat1;
+            ViewBag.subcat2 = subcat2;
             return View(db.Products.Find(id));
         }
 
@@ -39,15 +58,66 @@ namespace Webshop.Controllers
             return View();
         }
 
-        public ActionResult SideBar()
+        public ActionResult SideBar(string category = "", string subcat1 = "", string subcat2 = "")
         {
             var categories = db.Products.Select(p => p.Category).Distinct().ToList();
-            StringBuilder sb = new StringBuilder(categories.Count * 10);
+            StringBuilder sb = new StringBuilder("<table id=\"category_table\">",categories.Count * 10);
             foreach (var c in categories)
             {
+                sb.Append("<tr>");
+                sb.Append("<td>");
+                sb.Append("<a href=\"/Home?category=");
+                sb.Append(HttpUtility.HtmlEncode(c));
+                sb.Append("\">");
                 sb.Append(c);
-                sb.Append("<br />");
+                sb.Append("</a>");
+                sb.Append("</td>");
+                sb.Append("</tr>");
+                if (c.Equals(category))
+                {
+                    var subCategories1Products = db.Products.Where(p => p.Category.Equals(category));
+                    var subCategories1 = subCategories1Products.Select(p => p.SubCat1).Distinct().ToList();
+                    foreach (var sc1 in subCategories1)
+                    {
+                        sb.Append("<tr>");
+                        sb.Append("<td>");
+                        sb.Append("<a class=\"subcat1\" href=\"/Home?category=");
+                        sb.Append(HttpUtility.HtmlEncode(c));
+                        sb.Append("&subcat1=");
+                        sb.Append(HttpUtility.HtmlEncode(sc1));
+                        sb.Append("\">");
+                        sb.Append(sc1);
+                        sb.Append("</a>");
+                        sb.Append("</td>");
+                        sb.Append("</tr>");
+                        if (sc1.Equals(subcat1))
+                        {
+                            var subCategories2Products = subCategories1Products.Where(p => p.SubCat1.Equals(sc1));
+                            var subCategories2 = subCategories2Products.Select(p => p.SubCat2).Distinct().ToList();
+                            foreach (var sc2 in subCategories2)
+                            {
+                                if(sc2.Equals("")){ //blegh
+                                    continue;
+                                }
+                                sb.Append("<tr>");
+                                sb.Append("<td>");
+                                sb.Append("<a class=\"subcat2\" href=\"/Home?category=");
+                                sb.Append(HttpUtility.HtmlEncode(c));
+                                sb.Append("&subcat1=");
+                                sb.Append(HttpUtility.HtmlEncode(sc1));
+                                sb.Append("&subcat2=");
+                                sb.Append(HttpUtility.HtmlEncode(sc2));
+                                sb.Append("\">");
+                                sb.Append(sc2);
+                                sb.Append("</a>");
+                                sb.Append("</td>");
+                                sb.Append("</tr>");
+                            }
+                        }
+                    }
+                }
             }
+            sb.Append("</table>");
             return Content(sb.ToString());
         }
     }
